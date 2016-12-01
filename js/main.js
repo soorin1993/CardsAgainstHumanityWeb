a8def8f5525084b04d1a4b649ce0c3d369a367b9
Soo Park <soorin1993@gmail.com>
2016-12-01 15:35:35 -0700

    working without animation

--- js/main.js
+++ js/main.js
@@ -8,15 +8,14 @@ $(document).ready(function(){
 		foo = data["blackCards"];
 		$.each(foo, function(index, item) {
 			temp = item["text"]
-			console.log(temp);
 			questions.push(temp)
 		})
 		
 		bar = data["whiteCards"];
 		$.each(bar, function(index, item) {
-			temp = item["text"]
-			answers.push(temp)
+			answers.push(item)
 		})
+		
 		assignCards()
     });
 	
@@ -25,9 +24,49 @@ $(document).ready(function(){
 		item.innerHTML = "Cards<br> Against<br> Humanity";
 	});
 	
-    $(".card").click(function(){
-	    $(".back", this).toggle();  
-	    $(".front", this).toggle();  
+    $(".card").click(function(index, item){
+	    	
+	    var whichCard = $(this).parent().attr('class');
+	    
+	    if (whichCard == "questions") {
+	    	console.log(this)
+		    $(".questions .card .front").each(function(index, item) {
+			   if($(item).is(":visible")) {
+				   $(this).toggle();
+				   $(this).siblings('.back').toggle();
+				   $(this).parent().css({	"-moz-box-shadow": "",
+									"-webkit-box-shadow": "",
+									"box-shadow": ""}); 
+			   } 
+		    });
+			$(".back", this).toggle();  
+			$(".front", this).css("display", "block");
+			
+			$(this).css({	"-moz-box-shadow": "0 0 4px 6px #FF2A00",
+							"-webkit-box-shadow": "0 0 6px 2px #FF2A00",
+							"box-shadow": "0 0 6px 2px #FF2A00"}); 
+	    }
+	    
+		else {
+				    
+		    $(".answers .card .front").each(function(index, item) {
+			   if($(item).is(":visible")) {
+				   $(this).toggle();
+				   $(this).siblings('.back').toggle();
+				   $(this).parent().css({	"-moz-box-shadow": "",
+										"-webkit-box-shadow": "",
+										"box-shadow": ""}); 
+			   } 
+		    });
+
+			$(".back", this).toggle();  
+			$(".front", this).toggle();
+			$(this).css({	"-moz-box-shadow": "0 0 4px 6px #FF2A00",
+							"-webkit-box-shadow": "0 0 6px 2px #FF2A00",
+							"box-shadow": "0 0 6px 2px #FF2A00"}); 
+	    }
+
+ 
     });
 });
 
@@ -43,16 +82,16 @@ function assignCards() {
 	var qTemp = [];
 	var aTemp = [];
 	
-	$(".front").each(function(index, item) {
+	$(".questions .front").each(function(index, item) {
 		var randomNum = Math.floor(Math.random() * questions.length) + 1;
 		randomNum -= 1
-		console.log(randomNum);
-		console.log(questions[randomNum]);
 		item.innerHTML = questions[randomNum];
 	});
 	
-	for(var i = 0; i < 5; i++) {
-		aTemp.push(Math.floor(Math.random() * answers.length) + 1)
-	}
-	
+	$(".answers .front").each(function(index, item) {
+		var randomNum = Math.floor(Math.random() * answers.length) + 1;
+		randomNum -= 1
+		item.innerHTML = answers[randomNum];
+	});
+
 }
