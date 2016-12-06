d82a1b8ffc3e5454c20fe18645204842e8fe5684
Soo Park <soorin1993@gmail.com>
2016-12-06 15:36:47 -0700

    some done

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ var answers = [];
 
 var selectedQ;
 var selectedA;
+var userName;
+
+var fireData = [];
 
 $(document).ready(function(){
 	
@@ -39,7 +42,11 @@ $(document).ready(function(){
 		});
 		
 		$("#submit").click(function() {
-			firebaseData();
+			submitCards();
+		}); 
+		
+		$("#score-board").click(function() {
+			loadScores();
 		}); 
     });
 	
@@ -124,16 +131,123 @@ function assignCards() {
 
 }
 
-function firebaseData() {
-	
+function submitCards() {
+
 	$("#name-modal").css("display", "block");
+	$("#enter").click(function() {
+		$("#name-modal").css("display", "none");	
+		userName = $("#nameText").val();
+		firebaseData();
+	});
+	
+	$("#exit").click(function() {
+		$("#name-modal").css("display", "none");
+	});
+}
+
+function firebaseData() {
 	
 	var database = firebase.database();
 	var storageRef = database.ref();
 	
 	console.log(selectedQ);	
 	console.log(selectedA);
-	storageRef.push({name: name, email: email, team: team, payment: payment});
+	console.log(userName);
+	
+	storageRef.push({name: userName, question: selectedQ, answer: selectedA, score: 0});
 
+}
+
+function loadScores() {
+
+	$("#score-container").css("display", "block");
+
+	$("#exit_score").click(function() {
+		$("#score-container").css("display", "none");
+	});
+
+	$("#score").empty();
+
+	var database = firebase.database();
+	var storageRef = database.ref();
+	var scoreList = document.getElementById("score");
+	
+	storageRef.orderByChild("score").on("child_added", function(snapshot){
+
+		var name = snapshot.val().name;
+		var questionData = snapshot.val().question;
+		var answerData = snapshot.val().answer;
+		var scoreVal = snapshot.val().score;
+
+		var item = document.createElement('li');
+
+		var questionCard = document.createElement('div');
+		var questionText = document.createElement('p');
+		questionText.innerHTML = questionData;
+		questionCard.appendChild(questionText);
+		questionCard.className = "question_score";
+		
+		var answerCard = document.createElement('div');
+		var answerText = document.createElement('p');	
+		answerText.innerHTML = answerData;
+		answerCard.appendChild(answerText);
+		answerCard.className = "answer_score";
+		
+		
+		var displayOther = document.createElement('div');
+		
+		var displayUserName = document.createElement('p');
+		displayUserName.innerHTML = name;
+		
+		var displayScore = document.createElement('p');
+		displayScore.innerHTML = scoreVal;
+		
+		var voteButton = document.createElement('span');
+		voteButton.innerHTML = "VOTE";
+		
+		displayOther.appendChild(displayUserName);
+		displayOther.appendChild(displayScore);
+		displayOther.appendChild(voteButton);
+		displayOther.className = "other_score";
+		
+		item.appendChild(questionCard);
+		item.appendChild(answerCard);
+		item.appendChild(displayOther);
+
+		scoreList.appendChild(item);	
+		
+			
+	});		
+	
+
+	
+	
+	setTimeout(function(){
+		$('#score').append($('ol').find('li').get().reverse());
+		$("#score").css("display", "block");
+
+/*
+		$("#score").css({"display": "block", "padding": "0"});
+		
+		$("#score li").each(function(index, item) {
+// 			$(item).css({"width" : "100%", "height": "200px", "background-color": "#F1F0D8", "border-radius":"5px", "padding":"5px"});
+			$(item).children().each(function(index, item) {
+				if (index == 0) {
+				$(item).css({"width": "120px", "height": "170px", "background-color": "black", "color": "white", "border-radius": "5px", "padding": "10px", "float": "left", "margin":"5px 10px 0 10px"});
+				}
+				else if (index == 1) {
+					$(item).css({"width": "120px", "height": "170px", "background-color": "white", "color": "black", "border-radius": "5px", "padding": "10px", "float": "left"});
+				}
+				
+				else {
+					$(item).css({"width": "120px", "height": "170px", "background-color": "white", "color": "black", "border-radius": "5px", "padding": "10px", "float": "left"});
+				}			
+			});
+		});
+*/
+
+	}, 1000);
+	
+	
 }
 
